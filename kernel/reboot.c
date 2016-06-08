2a18e718de9400107416ebba63c5345d7d1f8712
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2016-06-07 23:39:33 -0500

    silence "Power down msg"

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -261,7 +261,7 @@ void kernel_power_off(void)
 		pm_power_off_prepare();
 	migrate_to_reboot_cpu();
 	syscore_shutdown();
-	pr_emerg("Power down\n");
+	//pr_emerg("Power down\n");
 	kmsg_dump(KMSG_DUMP_POWEROFF);
 	machine_power_off();
 }
